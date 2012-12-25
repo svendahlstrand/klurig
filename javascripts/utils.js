a9fd2d04a8475237340823b901573269caaddcc8
Sven Dahlstrand <sven.dahlstrand@gmail.com>
2012-12-25 11:52:06 +0100

    Add documentation to utils.

--- javascripts/utils.js
+++ javascripts/utils.js
@@ -1,8 +1,10 @@
 var utils = {
+  // Makes a deep copy (clone) of an object.
   deepCopy: function (obj) {
     return JSON.parse(JSON.stringify(obj));
   },
 
+  // Returns true if two objects represents the same data.
   equals: function (obj1, obj2) {
     return JSON.stringify(obj1) == JSON.stringify(obj2);
   }
