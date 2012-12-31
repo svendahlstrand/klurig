c438b03418486636aa38ef225b8a9c699bc0c469
Sven Dahlstrand <sven.dahlstrand@gmail.com>
2012-12-31 11:59:06 +0100

    Use the original prototype object so we don't loose the constructor property.

--- javascripts/klurig.js
+++ javascripts/klurig.js
@@ -31,84 +31,82 @@ var Board = function () {
   this.observers = [];
 };
 
-Board.prototype = {
-  // Get the tile at the provided position using [Algebraic notation](http://en.wikipedia.org/wiki/Algebraic_notation).
-  getTile: function (position) {
-    var row = position.charCodeAt(0) - 65;
-    var column = position[1] - 1;
+// Get the tile at the provided position using [Algebraic notation](http://en.wikipedia.org/wiki/Algebraic_notation).
+Board.prototype.getTile = function (position) {
+  var row = position.charCodeAt(0) - 65;
+  var column = position[1] - 1;
 
-    return this.state[row][column];
-  },
+  return this.state[row][column];
+};
 
-  // Set a tile at the provided position using [Algebraic notation](http://en.wikipedia.org/wiki/Algebraic_notation).
-  // The boards current tile color is used.
-  setTile: function (position) {
-    var row = position.charCodeAt(0) - 65;
-    var column = position[1] - 1;
+// Set a tile at the provided position using [Algebraic notation](http://en.wikipedia.org/wiki/Algebraic_notation).
+// The boards current tile color is used.
+Board.prototype.setTile = function (position) {
+  var row = position.charCodeAt(0) - 65;
+  var column = position[1] - 1;
 
-    if (this.state[row][column] != Tile.EMPTY) {
-      this.state[row][column] = this.currentTileColor;
-      this.notifyObservers();
-    }
-  },
+  if (this.state[row][column] != Tile.EMPTY) {
+    this.state[row][column] = this.currentTileColor;
+    this.notifyObservers();
+  }
+};
 
-  // Prepares and loads a puzzle as a two-dimensional array of integers.
-  prepare: function (puzzle) {
-    this.puzzle = puzzle;
-    this.state = utils.deepCopy(puzzle);
-    this.currentTileColor = Tile.RED;
+// Prepares and loads a puzzle as a two-dimensional array of integers.
+Board.prototype.prepare = function (puzzle) {
+  this.puzzle = puzzle;
+  this.state = utils.deepCopy(puzzle);
+  this.currentTileColor = Tile.RED;
 
-    var colors = {};
+  var colors = {};
 
-    // Reset board state to show just white tiles.
-    for (var row = 0; row < this.state.length; row++) {
-      for (var column = 0; column < this.state[row].length; column++) {
-        if (this.state[row][column] > Tile.EMPTY) {
-          colors[this.state[row][column]] = true;
-          this.state[row][column] = Tile.WHITE;
-        }
+  // Reset board state to show just white tiles.
+  for (var row = 0; row < this.state.length; row++) {
+    for (var column = 0; column < this.state[row].length; column++) {
+      if (this.state[row][column] > Tile.EMPTY) {
+        colors[this.state[row][column]] = true;
+        this.state[row][column] = Tile.WHITE;
       }
     }
+  }
 
-    this.colors = Object.keys(Tile).slice(0, Object.keys(colors).length);
+  this.colors = Object.keys(Tile).slice(0, Object.keys(colors).length);
 
-    this.notifyObservers('preparedPuzzle');
-  },
+  this.notifyObservers('preparedPuzzle');
+};
 
 
-  // Checks if the puzzle is solved (state equals puzzle).
-  isSolved: function () {
-    var normalizedState = utils.deepCopy(this.state);
+// Checks if the puzzle is solved (state equals puzzle).
+Board.prototype.isSolved = function () {
+  var normalizedState = utils.deepCopy(this.state);
 
-    var connections = [];
+  var connections = [];
 
-    for (var row = 0; row < normalizedState.length; row++) {
-      for (var column = 0; column < normalizedState[row].length; column++) {
-        if (normalizedState[row][column] == Tile.WHITE) { return false; }
-        connections[normalizedState[row][column]] = this.puzzle[row][column];
-      }
+  for (var row = 0; row < normalizedState.length; row++) {
+    for (var column = 0; column < normalizedState[row].length; column++) {
+      if (normalizedState[row][column] == Tile.WHITE) { return false; }
+      connections[normalizedState[row][column]] = this.puzzle[row][column];
     }
+  }
 
-    for (var row = 0; row < normalizedState.length; row++) {
-      for (var column = 0; column < normalizedState[row].length; column++) {
-        normalizedState[row][column] = connections[normalizedState[row][column]];
-      }
+  for (var row = 0; row < normalizedState.length; row++) {
+    for (var column = 0; column < normalizedState[row].length; column++) {
+      normalizedState[row][column] = connections[normalizedState[row][column]];
     }
+  }
 
-    return utils.equals(normalizedState, this.puzzle);
-  },
+  return utils.equals(normalizedState, this.puzzle);
+};
 
-  // Add observer to this object.
-  addObserver: function (observer, context) {
-    this.observers.push({block: observer, context: context || null});
-  },
+// Add observer to this object.
+Board.prototype.addObserver = function (observer, context) {
+  this.observers.push({block: observer, context: context || null});
+};
 
-  // Notify observers of this object about a change.
-  notifyObservers: function () {
-    for (var i = 0; i < this.observers.length; i++) {
-      var observer = this.observers[i];
-      observer.block.apply(observer.context, arguments);
-    }
+// Notify observers of this object about a change.
+Board.prototype.notifyObservers = function () {
+  for (var i = 0; i < this.observers.length; i++) {
+    var observer = this.observers[i];
+    observer.block.apply(observer.context, arguments);
   }
 };
 
@@ -152,43 +150,41 @@ var BoardView = function (board, controller) {
   }, this);
 };
 
-BoardView.prototype = {
-  renderInitial: function () {
-    // Render the initial view.
-    var html = '';
-    for (var row = 0; row < this.board.state.length; row++) {
-      for (var column = 0; column < this.board.state[row].length; column++) {
-        var tile = this.board.state[row][column];
-        var position = String.fromCharCode(row + 65) + (column + 1);
-        var classValue = column === 0 ? 'first ' : '';
+BoardView.prototype.renderInitial = function () {
+  // Render the initial view.
+  var html = '';
+  for (var row = 0; row < this.board.state.length; row++) {
+    for (var column = 0; column < this.board.state[row].length; column++) {
+      var tile = this.board.state[row][column];
+      var position = String.fromCharCode(row + 65) + (column + 1);
+      var classValue = column === 0 ? 'first ' : '';
 
-        if (tile == Tile.EMPTY) {
-          classValue += 'slot';
-        }
-        else {
-          classValue += 'tile color-' + tile;
-        }
-
-        html += '<div class="' + classValue + '" data-position="' + position + '"></div>';
+      if (tile == Tile.EMPTY) {
+        classValue += 'slot';
       }
+      else {
+        classValue += 'tile color-' + tile;
+      }
+
+      html += '<div class="' + classValue + '" data-position="' + position + '"></div>';
     }
-    this.canvas.innerHTML = html;
-  },
+  }
+  this.canvas.innerHTML = html;
+};
 
-  render: function () {
-    var tiles = this.canvas.getElementsByTagName('div');
-    for (var i = 0; i < tiles.length; i++) {
-      var tile = tiles[i];
+BoardView.prototype.render = function () {
+  var tiles = this.canvas.getElementsByTagName('div');
+  for (var i = 0; i < tiles.length; i++) {
+    var tile = tiles[i];
 
-      if (!tile.classList.contains('tile')) { continue; }
+    if (!tile.classList.contains('tile')) { continue; }
 
-      var position = tile.attributes['data-position'].value;
-      tile.setAttribute('class', tile.getAttribute('class').replace(/color-\d+/, 'color-' + this.board.getTile(position)));
-    }
+    var position = tile.attributes['data-position'].value;
+    tile.setAttribute('class', tile.getAttribute('class').replace(/color-\d+/, 'color-' + this.board.getTile(position)));
+  }
 
-    if (this.board.isSolved()) {
-      alert('Win!');
-    }
+  if (this.board.isSolved()) {
+    alert('Win!');
   }
 };
 
@@ -213,14 +209,12 @@ var TileColorsView = function (board, controller) {
   }, this);
 };
 
-TileColorsView.prototype = {
-  render: function () {
-    var html = '';
-    this.board.colors.forEach(function (color) {
-      html += '<li><a href="#" data-value="' + color + '">' + color + '</a></li>';
-    });
-    this.canvas.innerHTML = html;
-  }
+TileColorsView.prototype.render = function () {
+  var html = '';
+  this.board.colors.forEach(function (color) {
+    html += '<li><a href="#" data-value="' + color + '">' + color + '</a></li>';
+  });
+  this.canvas.innerHTML = html;
 };
 
 // ### Puzzles
@@ -247,18 +241,16 @@ var GameController = function (puzzles, board) {
   this.board = board;
 };
 
-GameController.prototype = {
-  handleInteraction: function (position) {
-    this.board.setTile(position);
-  },
+GameController.prototype.handleInteraction = function (position) {
+  this.board.setTile(position);
+};
 
-  changeTileColor: function (color) {
-    this.board.currentTileColor = Tile[color];
-  },
+GameController.prototype.changeTileColor = function (color) {
+  this.board.currentTileColor = Tile[color];
+};
 
-  prepareBoard:  function (number) {
-    this.board.prepare(this.puzzles[number]);
-  }
+GameController.prototype.prepareBoard = function (number) {
+  this.board.prepare(this.puzzles[number]);
 };
 
 // Application
